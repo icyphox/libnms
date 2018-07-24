2e738b474384b41eea92af8004d4552ed15f2315
Anirudh <icyph0x@pm.me>
2018-07-24 19:56:56 +0530

    feat: rest of em

--- src/nms.nim
+++ src/nms.nim
@@ -7,3 +7,7 @@ const libnmsdll = "lib/libnms.so"
 proc nms_exec*(s: cstring): cstring {.libnms.}
 proc nms_set_auto_decrypt*(i: cint) {.libnms.}
 proc nms_set_foreground_color*(s: cstring) {.libnms.}
+proc nms_set_clear_scr*(i: cint) {.libnms.}
+proc nms_use_color*(i: cint) {.libnms.}
+proc nms_set_return_opts*(s: cstring) {.libnms.}
+proc nms_set_input_position*(i1: cint, i2: cint) {.libnms.}
