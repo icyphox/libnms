f6ce699a12d753470e261f713dc3ca8b5d397708
Anirudh <icyph0x@pm.me>
2018-07-24 19:43:27 +0530

    feat: define pragma to load lib

--- src/nms.nim
+++ src/nms.nim
@@ -1,4 +1,9 @@
+# path to libnms.so
+const libnmsdll = "lib/libnms.so"
+
+{.pragma: libnms, importc, dynlib: libnmsdll.}
+
 # procs from libnms.so
-proc nms_exec(s: cstring): cstring {.importc, dynlib: "/usr/local/lib/libnms.so".}
-proc nms_set_auto_decrypt(i: cint) {.importc, dynlib: "/usr/local/lib/libnms.so".}
-proc nms_set_foreground_color(s: cstring) {.importc, dynlib: "/usr/local/lib/libnms.so".}
+proc nms_exec*(s: cstring): cstring {.libnms.}
+proc nms_set_auto_decrypt*(i: cint) {.libnms.}
+proc nms_set_foreground_color*(s: cstring) {.libnms.}
